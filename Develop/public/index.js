d017adbc996beb841e485ab30b6c890a9e7c812f
Maxsem <max.semegen@gmail.com>
2020-06-05 08:59:28 -0400

    routes files and mongo js

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -2,12 +2,12 @@ init();
 
 async function init() {
   if (location.search.split("=")[1] === undefined) {
+    console.log("excercise");
     const workout = await API.getLastWorkout();
     if (workout) {
       location.search = "?id=" + workout._id;
     } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
+      document.querySelector("#continue-btn").classList.add("d-none");
     }
   }
 }
-
